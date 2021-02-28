8382f9f0ccf010fb9342e6e6a2746d1febc27ed4
stacksagar <secure.js.info@gmail.com>
2021-02-28 07:20:17 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -13,36 +13,32 @@ export default class App extends Component {
         
         <nav> 
           <ul> 
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Home *</Link></li>
             <li><Link to="/blog">Blog</Link></li>
             <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/about">About</Link></li>
           </ul>
         </nav>
 
-        <Route exact path={`/`} >
-          <Home />
-        </Route>
+          <Switch>
+          <Route exact path={`/`} >
+            <Home />
+          </Route>
 
-        <Route exact path="/about" >
-          <Home />
-          <About />
-        </Route>
+          <Route exact path="/about" >
+            <Home />
+            <About />
+          </Route>
 
-        <Route exact path="/blog" > 
-          <Blog />
-        </Route>
+          <Route exact path="/blog" > 
+            <Blog />
+          </Route>
 
-        <Route exact path="/contact" >
-          <Home />
-          <Contact />
-        </Route>
-{/* 
-        <Route exact path={`/about`} component={About} />
-        <Route exact path={`/blog`} render={(routerProps) => < Blog routerProps={routerProps} />} />
-        <Route exact path={`/`} render={(routerProps) => < Home routerProps={routerProps} />} />
-        <Route exact path={`/contact`} render={(routerProps) => < Contact routerProps={routerProps} />} />    
-        */}
+          <Route exact path="/contact" >
+            <Home />
+            <Contact />
+          </Route> 
+        </Switch>
 
       </div>     
     );
